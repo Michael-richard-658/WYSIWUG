3b2cc234d199b5ae4a78eabafab2f481702b08cd
Michael Richard <michaelrichard7020@gmail.com>
2025-02-27 10:19:34 +0530

    sizing

---
+++ src/app/components/header-components/top.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './header.css'
+function Top() {
+  return (
+    <div className='top-div'>
+      ok4
+    </div>
+  )
+}
+
+export default Top
---
+++ src/app/components/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Top from './header-components/top'
+
+function Header() {
+  return (
+    <div>
+      <Top />
+    </div>
+  )
+}
+
+export default Header
